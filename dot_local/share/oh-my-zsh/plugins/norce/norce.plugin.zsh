9bc6b3770ff69e674fa6006205666af8f2642b79
Thomas Johansen <thomasjo@gmail.com>
2022-06-02 18:51:57 +0200

    Add custom work plugin for omz

---
+++ dot_local/share/oh-my-zsh/plugins/norce/norce.plugin.zsh
@@ -0,0 +1,11 @@
+function kill-vpn () {
+  # TODO: Refactor away from legacy `remove` command.
+  launchctl remove com.paloaltonetworks.gp.pangps
+  launchctl remove com.paloaltonetworks.gp.pangpa
+}
+
+function reactivate-vpn () {
+  # TODO: Refactor away from legacy `load` command.
+  launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangpa.plist
+  launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangps.plist
+}
