c8186518c7697998ff8fcef5ac13e2091127ddc6
Thomas Johansen <thomasjo@gmail.com>
2022-08-23 23:35:35 +0200

    Convert "norce" plugin to tab indentation

--- dot_local/share/oh-my-zsh/plugins/norce/norce.plugin.zsh
+++ dot_local/share/oh-my-zsh/plugins/norce/norce.plugin.zsh
@@ -1,11 +1,11 @@
 function kill-vpn () {
-  # TODO: Refactor away from legacy `remove` command.
-  launchctl remove com.paloaltonetworks.gp.pangps
-  launchctl remove com.paloaltonetworks.gp.pangpa
+	# TODO: Refactor away from legacy `remove` command.
+	launchctl remove com.paloaltonetworks.gp.pangps
+	launchctl remove com.paloaltonetworks.gp.pangpa
 }
 
 function reactivate-vpn () {
-  # TODO: Refactor away from legacy `load` command.
-  launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangpa.plist
-  launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangps.plist
+	# TODO: Refactor away from legacy `load` command.
+	launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangpa.plist
+	launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangps.plist
 }
