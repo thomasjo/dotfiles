581e824e4d97630ad842b7208a9d8f101a1dacdc
Thomas Johansen <thomasjo@gmail.com>
2023-03-14 18:33:21 +0100

    Modernize the "VPN killer" utility
    This scripts combines the two separate VPN functions for killing and
    reactivating the GlobalProtect VPN client into a single function. The
    new `vpn` function supports two commands: `kill` and `revive`.
    
    Besides some additional quality of life stuff like a usage description,
    the old functionality remains identical.

--- dot_local/share/oh-my-zsh/plugins/norce/norce.plugin.zsh
+++ dot_local/share/oh-my-zsh/plugins/norce/norce.plugin.zsh
@@ -1,11 +1,35 @@
-function kill-vpn () {
-	# TODO: Refactor away from legacy `remove` command.
-	launchctl remove com.paloaltonetworks.gp.pangps
-	launchctl remove com.paloaltonetworks.gp.pangpa
-}
-
-function reactivate-vpn () {
-	# TODO: Refactor away from legacy `load` command.
-	launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangpa.plist
-	launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangps.plist
+function vpn() {
+	local usage="Usage: "$0" [kill|revive]"
+	case $# in
+		0)
+			echo "$usage"
+			return 1
+			;;
+		1)
+			case $1 in
+				kill)
+					echo "Killing VPN client..."
+					launchctl remove com.paloaltonetworks.gp.pangpa
+					launchctl remove com.paloaltonetworks.gp.pangps
+					echo "Done!"
+					;;
+				revive)
+					echo "Reviving VPN client..."
+					launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangpa.plist
+					launchctl load /Library/LaunchAgents/com.paloaltonetworks.gp.pangps.plist
+					echo "Done!"
+					;;
+				*)
+					echo "'$1' is not a valid command."
+					echo "$usage"
+					return 1
+					;;
+			esac
+			;;
+		*)
+			echo "Too many arguments provided ($#)."
+			echo "$usage"
+			return 1
+			;;
+	esac
 }
