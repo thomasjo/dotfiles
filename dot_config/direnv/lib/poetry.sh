182119ffd96c0efc22d5ae10bd606b758b9e4a6a
Thomas Johansen <thomasjo@gmail.com>
2023-10-17 15:46:42 +0200

    Add layout for PDM

--- dot_config/direnv/lib/poetry.sh
+++ dot_config/direnv/lib/poetry.sh
@@ -1,3 +1,26 @@
+layout_pdm() {
+  if [[ ! -f "pyproject.toml" ]]; then
+    log_error 'No pyproject.toml found. Use `pdm init` to create one first.'
+    exit 2
+  fi
+
+  # Try to short-circuit the need to invoke pdm, which is slow.
+  if [[ -d ".venv" ]]; then
+    VIRTUAL_ENV="$(pwd)/.venv"
+  else
+    VIRTUAL_ENV="$(pdm venv list | grep "^\*"  | awk -F" " '{print $3}')"
+  fi
+
+  if [[ -z $VIRTUAL_ENV || ! -d $VIRTUAL_ENV ]]; then
+    log_error 'No pdm virtual environment found. Use `pdm install` to create one first.'
+    exit 2
+  fi
+
+  PATH_add "$VIRTUAL_ENV/bin"
+  export PDM_ACTIVE=1
+  export VIRTUAL_ENV
+}
+
 layout_poetry() {
   if [[ ! -f "pyproject.toml" ]]; then
     log_error 'No pyproject.toml found. Use `poetry new` or `poetry init` to create one first.'
